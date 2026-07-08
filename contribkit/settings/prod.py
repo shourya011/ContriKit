df8c843353464238b4209776e600431ec269e657
Shourya Sharma <shouryano01@gmail.com>
2026-07-08 20:33:39 +0530

    Change Settings

--- contribkit/settings/prod.py
+++ contribkit/settings/prod.py
@@ -37,7 +37,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
